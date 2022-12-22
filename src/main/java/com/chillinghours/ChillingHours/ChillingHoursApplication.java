56e4695b9c497917eff60319894f420c31384cd1
jessematty <jamatty@gmail.com>
2022-12-22 13:11:10 -0500

    Initial commit

---
+++ src/main/java/com/chillinghours/ChillingHours/ChillingHoursApplication.java
@@ -0,0 +1,13 @@
+package com.chillinghours.ChillingHours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChillingHoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChillingHoursApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chillinghours/ChillingHours/ChillingHoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chillinghours.ChillingHours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChillingHoursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
